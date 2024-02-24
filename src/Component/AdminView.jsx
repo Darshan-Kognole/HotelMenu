30b86455f3287707f1e9084e6dfd518c2cc794e9
Darshan-Kognole <darshankognole001@gmail.com>
2024-02-24 22:45:15 +0530

    Adding view page

--- src/Component/AdminView.jsx
+++ src/Component/AdminView.jsx
@@ -1,93 +1,39 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import '../Style/AdminView.css'
+import StarIcon from '@mui/icons-material/Star';
 
 const AdminView = () => {
+  let[data,setdata]=useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:1000/Product")
+    .then((res)=>{
+      console.log(res.data);
+      setdata(res.data)
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[])
   return (
-    <div>
-       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> 
-
-    <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=f5f5f5"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=eee"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel> 
+    <div className='Addminviewpage'>
+       {data.map((x)=>{
+        return(
+          <div key={x.id} className='displayproduct'>
+            <div className='img'>
+              <img src={x.turl} alt="" />
+            </div>
+            <div className='restaurant'>
+              <h5>{x.Rstname} </h5>
+              <h4>{x.rating} <StarIcon id="star"/></h4>
+            </div>
+              <h2 className='dname'>{x.Dname}</h2>
+          </div>
+          
+        )
+       })}
     </div>
-  )
+  );
 }
 
 export default AdminView
